c694bf6d5aff0f74fd3c0419a4284a4209b79e84
Yash Gupta <yash16022001@gmail.com>
2023-07-15 23:13:08 +0530

    backend completed

--- srcTs/server.ts
+++ srcTs/server.ts
@@ -2,14 +2,11 @@ import { config } from "dotenv";
 import express, { Application } from "express";
 import appDataSource from "./Database/DataSource";
 import { trainRoutes } from "./Routes/train.routes";
-import path from "path";
 
 config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
-const allowedOrigins = process.env.ALLOWED_HOSTS;
-console.log(allowedOrigins);
 
 const startServer = async () => {
   try {
@@ -23,85 +20,9 @@ const startServer = async () => {
     console.log("Unable to connect to database", error);
   }
 };
-app.use(express.static(__dirname + "/../build"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 startServer();
 
 app.use("/train", trainRoutes);
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-});
-
-// app.get("/seedSeats", async (req: Request, res: Response) => {
-//   const trainRepo = appDataSource.getRepository(TrainEntity);
-//   const seatsRepo = appDataSource.getRepository(SeatsEntity);
-
-//   const train = await trainRepo.findOne({
-//     where: {
-//       id: 1,
-//     },
-//   });
-
-//   try {
-//     for (let index = 0; index < 70; index++) {
-//       const seat = new SeatsEntity();
-//       seat.seatNumber = index + 1;
-//       seat.isAvailable = true;
-//       seat.isBooked = false;
-//       seat.trainName = train!.trainName;
-//       seat.train = train!;
-//       await seatsRepo.save(seat);
-//     }
-//   } catch (error) {
-//     return res.send("Unable to seed seats");
-//   }
-//   const seats = await seatsRepo.find();
-//   res.send(seats);
-// });
-
-// app.get("/drop", async (req: Request, res: Response) => {
-//   try {
-//     await dropTable();
-//     res.send("Successful");
-//   } catch (error) {
-//     console.log("Unable to drop table", error);
-//     res.send("Unable to drop table");
-//   }
-// });
-
-// const createTable = async () => {
-//   try {
-//     await appDataSource.transaction(async (txn) => {
-//       try {
-//         const trainRepo = txn.getRepository(TrainEntity);
-//         const seatsRepo = txn.getRepository(SeatsEntity);
-//         const train = new TrainEntity();
-//         train.trainName = "Shatabdi Express";
-//         train.fromStation = "Delhi";
-//         train.toStation = "Mumbai";
-//         train.departureTime = moment().utc().toDate();
-//         train.arrivalTime = moment().utc().toDate();
-//         train.seatsAvailableCount = 70;
-//         train.seatsBookedCount = 0;
-//         train.waitingCount = 0;
-//         const savedTrain = await trainRepo.save(train);
-
-//         for (let index = 0; index < 70; index++) {
-//           const seat = new SeatsEntity();
-//           seat.seatNumber = index + 1;
-//           seat.isAvailable = true;
-//           seat.isBooked = false;
-//           seat.trainName = savedTrain.trainName;
-//           seat.train = savedTrain;
-//           await seatsRepo.save(seat);
-//         }
-//       } catch (error) {
-//         console.log("Unable to create table", error);
-//       }
-//     });
-//   } catch (error) {
-//     console.log("Unable to create table", error);
-//   }
-// };
