13dcf77cf1ed3b124cd8d432bc9f7ea6f9faaa33
Yash Gupta <yash16022001@gmail.com>
2023-07-15 23:10:10 +0530

    Assesment complete

--- src/Utils/request.utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { Response } from "express";
-export declare const successResponse: <T>(res: Response, data: T, code?: number) => Response<any, Record<string, any>>;
-export declare const errorResponse: (res: Response, errorMessage: string, code?: number) => Response<any, Record<string, any>>;
